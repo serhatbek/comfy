23df16228d4405c4de234030a7454f94ccf7ebf6
EXT02D52540 <serhat.bek@consultant.turkcell.com.tr>
2024-04-25 14:25:24 +0300

    create pages

---
+++ src/pages/index.ts
@@ -0,0 +1,11 @@
+export { default as HomeLayout } from './HomeLayout/HomeLayout';
+export { default as About } from './About/About';
+export { default as Landing } from './Landing/Landing';
+export { default as SingleProduct } from './SingleProduct/SingleProduct';
+export { default as Products } from './Products/Products';
+export { default as Cart } from './Cart/Cart';
+export { default as Error } from './Error/Error';
+export { default as Login } from './Login/Login';
+export { default as Register } from './Register/Register';
+export { default as Checkout } from './Checkout/Checkout';
+export { default as Orders } from './Orders/Orders';
