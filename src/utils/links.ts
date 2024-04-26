3a2691eda8946da53bba704b73d44988371279b0
serhatbek <serhat.bekk@gmail.com>
2024-04-26 17:33:16 +0300

    Links List

---
+++ src/utils/links.ts
@@ -0,0 +1,13 @@
+type Link = {
+  href: string;
+  label: string;
+};
+
+export const links: Link[] = [
+  { href: '/comfy/', label: 'home' },
+  { href: '/comfy/about', label: 'about' },
+  { href: '/comfy/products', label: 'products' },
+  { href: '/comfy/cart', label: 'cart' },
+  { href: '/comfy/checkout', label: 'checkout' },
+  { href: '/comfy/orders', label: 'orders' },
+];
