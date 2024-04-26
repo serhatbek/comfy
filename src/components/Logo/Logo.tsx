3f3510805dbe5f46421be2a63b11d66818017d37
serhatbek <serhat.bekk@gmail.com>
2024-04-26 17:27:21 +0300

    Logo Component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,4 +1,14 @@
+import { Armchair } from 'lucide-react';
+import { Link } from 'react-router-dom';
+
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <Link
+      to='/comfy/'
+      className='hidden lg:flex justify-center items-center bg-primary p-2 rounded-lg text-white'
+    >
+      <Armchair className='w-8 h-8' />
+    </Link>
+  );
 };
 export default Logo;
