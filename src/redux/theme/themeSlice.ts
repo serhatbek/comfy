a96e19659acbf3dfc6877bd8c568926717d6f955
serhatbek <serhat.bekk@gmail.com>
2024-04-29 12:40:36 +0300

    Theme Slice

--- src/redux/theme/themeSlice.ts
+++ src/redux/theme/themeSlice.ts
@@ -1,15 +1,32 @@
+import { applyTheme } from '@/utils';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export type Theme = 'dark' | 'light' | 'system';
 
-const initialState = {
-  name: 'user slice',
+const initialTheme = (): Theme => {
+  const theme = (localStorage.getItem('theme') as Theme) || 'system';
+  applyTheme(theme);
+  return theme;
+};
+
+type ThemeState = {
+  theme: Theme;
+};
+
+const initialState: ThemeState = {
+  theme: initialTheme(),
 };
 
 const themeSlice = createSlice({
   name: 'theme',
   initialState,
-  reducers: {},
+  reducers: {
+    setTheme: (state, action: PayloadAction<Theme>) => {
+      state.theme = action.payload;
+      applyTheme(action.payload);
+      localStorage.setItem('theme', action.payload);
+    },
+  },
 });
 
 export default themeSlice.reducer;
