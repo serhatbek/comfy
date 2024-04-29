f9a2e6dcb131e3f6132aeb483391b337b733b1f2
serhatbek <serhat.bekk@gmail.com>
2024-04-29 12:05:39 +0300

    NavLinks Component

--- src/components/NavLinks/NavLinks.tsx
+++ src/components/NavLinks/NavLinks.tsx
@@ -1,4 +1,26 @@
+import { links } from '@/utils';
+import { NavLink } from 'react-router-dom';
+
 const NavLinks = () => {
-  return <div>NavLinks</div>;
+  return (
+    <ul className='hidden lg:flex justify-center items-center gap-x-4'>
+      {links.map((link) => {
+        return (
+          <li key={link.label}>
+            <NavLink
+              to={link.href}
+              className={({ isActive }) => {
+                return `capitalize font-light tracking-wide ${
+                  isActive ? 'text-primary' : ''
+                }`;
+              }}
+            >
+              {link.label}
+            </NavLink>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 export default NavLinks;
