e2db0c1dde05f409c7694a995667eb5a0ee38b18
serhatbek <serhat.bekk@gmail.com>
2024-04-29 13:49:13 +0300

    CartButton Component

--- src/components/CartButton/CartButton.tsx
+++ src/components/CartButton/CartButton.tsx
@@ -1,4 +1,23 @@
+import { Button } from '@/components/ui/button';
+import { ShoppingCart } from 'lucide-react';
+import { Link } from 'react-router-dom';
+
 const CartButton = () => {
-  return <div>CartButton</div>;
+  const numItemsInCart = 5;
+  return (
+    <Button
+      asChild
+      variant='outline'
+      size='icon'
+      className='flex justify-center items-center relative'
+    >
+      <Link to='/comfy/cart'>
+        <ShoppingCart />
+        <span className='absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs'>
+          {numItemsInCart}
+        </span>
+      </Link>
+    </Button>
+  );
 };
 export default CartButton;
