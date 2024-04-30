8a56ef72d270b7128285a58f3671f282277a2f1b
serhatbek <serhat.bekk@gmail.com>
2024-04-30 10:56:32 +0300

    About Page Component

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,5 +1,19 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section>
+      <h1 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-tight sm:text-6xl'>
+        We love{' '}
+        <span className='bg-primary py-2 px-4 rounded-lg tracking-widest text-white'>
+          comfy
+        </span>
+      </h1>
+      <p className='mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum molestiae
+        laborum neque aperiam cum molestias id numquam nemo rem veniam enim,
+        aspernatur a cumque asperiores, error eveniet pariatur deserunt quasi.
+      </p>
+    </section>
+  );
 };
 
 export default About;
