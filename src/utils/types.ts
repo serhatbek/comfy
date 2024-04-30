b73b98045a302fe5b5bbad504528df3c92a6f1ba
serhatbek <serhat.bekk@gmail.com>
2024-04-30 11:54:49 +0300

    Custom Fetch Function

---
+++ src/utils/types.ts
@@ -0,0 +1,35 @@
+export type Product = {
+  id: number;
+  attributes: {
+    category: string;
+    company: string;
+    createdAt: string;
+    description: string;
+    featured: boolean;
+    image: string;
+    price: string;
+    publishedAt: string;
+    shipping: boolean;
+    title: string;
+    updatedAt: string;
+    colors: string[];
+  };
+};
+
+export type Pagination = {
+  page: number;
+  pageCount: number;
+  pageSize: number;
+  total: number;
+};
+
+export type ProductsMeta = {
+  categories: string[];
+  companies: string[];
+  pagination: Pagination;
+};
+
+export type ProductsResponse = {
+  data: Product[];
+  meta: ProductsMeta;
+};
