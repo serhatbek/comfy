a85e29dd417ad86c0dda5deeca5e8425249f8024
serhatbek <serhat.bekk@gmail.com>
2024-05-01 13:08:41 +0300

    Hero Component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,28 @@
+import { Link } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
+import { HeroCarousel } from '../../components';
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className='grid grid-cols-1 lg:grid-cols-2 gap-24 items-center'>
+      <div>
+        <h1 className='max-w-2xl text-4xl font-bold tracking-tight  sm:text-6xl '>
+          We’re changing the way people shop.
+        </h1>
+
+        <p className='mt-8 max-w-xl text-lg leading-8'>
+          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
+          cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat
+          aliqua. Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
+          qui lorem cupidatat commodo.
+        </p>
+
+        <Button asChild size='lg' className='mt-10'>
+          <Link to='/comfy/products'>Our Products</Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 };
 export default Hero;
