41fe99508367d7de1a2c88e8ae491995c7da388f
serhatbek <serhat.bekk@gmail.com>
2024-05-02 15:16:04 +0300

    Filters Component - Setup

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,4 +1,28 @@
+import { Form, useLoaderData, Link } from 'react-router-dom';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
+
 const Filters = () => {
-  return <div>Filters</div>;
+  return (
+    <Form className='border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
+      <div className='mb-2'>
+        <Label htmlFor='search'>Search Product</Label>
+        <Input id='search' name='search' type='text' defaultValue='' />
+      </div>
+      <Button type='submit' size='sm' className='self-end mb-2'>
+        search
+      </Button>
+      <Button
+        type='button'
+        asChild
+        size='sm'
+        variant='outline'
+        className='self-end mb-2'
+      >
+        <Link to='/comfy/products'>reset</Link>
+      </Button>
+    </Form>
+  );
 };
 export default Filters;
