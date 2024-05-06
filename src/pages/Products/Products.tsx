2c5b98aa9c0ce6d0f361c78b830bbcea1a543faf
serhatbek <serhat.bekk@gmail.com>
2024-05-06 13:09:35 +0300

    Query Params

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -4,8 +4,16 @@ import { type LoaderFunction } from 'react-router-dom';
 
 const url = '/products';
 
-export const loader: LoaderFunction = async (): Promise<ProductsResponse> => {
-  const response = await customFetch<ProductsResponse>(url);
+export const loader: LoaderFunction = async ({
+  request,
+}): Promise<ProductsResponse> => {
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+  console.log(params);
+  const response = await customFetch<ProductsResponse>(url, {
+    params,
+  });
   return { ...response.data };
 };
 
