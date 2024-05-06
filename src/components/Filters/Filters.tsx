deb007600bf33119b4ddfcb872772155d511e362
serhatbek <serhat.bekk@gmail.com>
2024-05-06 15:34:36 +0300

    Form Input 2

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,19 +1,21 @@
 import { Form, useLoaderData, Link } from 'react-router-dom';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
 import { ProductsResponseWithParams } from '@/utils';
+import { FormInput } from '../../components';
 
 const Filters = () => {
   const { meta, params } = useLoaderData() as ProductsResponseWithParams;
-  const { search } = params;
+  const { search, category, shipping, order, price } = params;
 
   return (
     <Form className='border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
-      <div className='mb-2'>
-        <Label htmlFor='search'>Search Product</Label>
-        <Input id='search' name='search' type='text' defaultValue={search} />
-      </div>
+      <FormInput
+        type='search'
+        label='search product'
+        name='search'
+        defaultValue={search}
+      />
+
       <Button type='submit' size='sm' className='self-end mb-2'>
         search
       </Button>
